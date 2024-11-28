1b8ea2f5481858dd6d2658fdded1d4050881cd8d
Eun6636 <tktmdmswl01@gmail.com>
2024-11-29 02:46:13 +0900

    Add : SkillManager Singleton

--- Assets/Script/Manager/SkillManager.cs
+++ Assets/Script/Manager/SkillManager.cs
@@ -2,9 +2,35 @@ using UnityEngine;
 
 public class SkillManager : MonoBehaviour
 {
+    private static SkillManager _instance;
+
+    public static SkillManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                // 씬에 SkillManager가 없으면 새로 생성
+                GameObject obj = new GameObject("SkillManager");
+                _instance = obj.AddComponent<SkillManager>();
+            }
+            return _instance;
+        }
+    }
+
     public CharacterJob[] Job; // 각 전직의 배열
 
-
+    private void Awake()
+    {
+        // 다른 SkillManager가 존재하면 파괴
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        _instance = this;
+        DontDestroyOnLoad(gameObject); // 씬 전환 시 파괴되지 않도록
+    }
 
     public void UseSkill1(int index, Vector2 position, Quaternion rotation)
     {
