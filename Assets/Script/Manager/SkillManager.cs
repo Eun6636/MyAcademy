a1a301f0aee8002ff5f0063bc0af2def3dcc2435
Eun6636 <tktmdmswl01@gmail.com>
2024-11-29 02:43:37 +0900

    Fix: nameFix

--- Assets/Script/Manager/SkillManager.cs
+++ Assets/Script/Manager/SkillManager.cs
@@ -2,23 +2,23 @@ using UnityEngine;
 
 public class SkillManager : MonoBehaviour
 {
-    public CharacterJob[] skills; // 각 전직의 스킬 배열
+    public CharacterJob[] Job; // 각 전직의 배열
 
 
 
     public void UseSkill1(int index, Vector2 position, Quaternion rotation)
     {
-        if (index < skills.Length)
+        if (index < Job.Length)
         {
-            skills[index].Skill1(position, rotation);
+            Job[index].Skill1(position, rotation);
         }
     }
 
     public void UseSkill2(int index, Vector2 position, Quaternion rotation)
     {
-        if (index < skills.Length)
+        if (index < Job.Length)
         {
-            skills[index].Skill1(position, rotation);
+            Job[index].Skill2(position, rotation);
         }
     }
 }
